f16b344c8f83182c010a9dea2f1f741178399d3e
JadejaVishwrajsinh7421 <JadejaVishwrajsinh7421>
2026-04-10 21:45:48 +0530

    "My Lab work of python during the Labs"

--- cook_code_py/functions&recursion_07/intro.py
+++ cook_code_py/functions&recursion_07/intro.py
@@ -1,2 +1,12 @@
 #functions --> block of code that performs a specicifc task
 
+# To make the code ease of reducant code  we use function 
+
+#def keyword is used
+
+def sum(a,b):
+    # we can return the output or print them in a function 
+    return a+b
+
+r
+
